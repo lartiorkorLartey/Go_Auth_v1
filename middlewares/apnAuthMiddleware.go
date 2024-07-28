b177055e2dca99a630f81c86a2f16916939a8616
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-07-28 12:35:21 +0000

    chore: update apn middleware

---
+++ middlewares/apnAuthMiddleware.go
@@ -0,0 +1,30 @@
+package middlewares
+
+import (
+    "authapp.com/m/initializers"
+    "authapp.com/m/models"
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func APNAuthMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        apnHeader := c.GetHeader("X-APN")
+
+        if apnHeader == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "APN header missing"})
+            c.Abort()
+            return
+        }
+
+        var client models.Client
+        if err := initializers.DB.Where("apn = ?", apnHeader).First(&client).Error; err != nil {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid APN"})
+            c.Abort()
+            return
+        }
+
+        c.Set("client", client)
+        c.Next()
+    }
+}
