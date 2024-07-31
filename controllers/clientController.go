c0ac3e996b7386182539273b28d7776924fcfa8c
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-07-31 01:20:05 +0000

    chore: add more controllers

--- controllers/clientController.go
+++ controllers/clientController.go
@@ -7,6 +7,7 @@ import (
 	"authapp.com/m/models"
 	"authapp.com/m/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -80,11 +81,186 @@ func ClientLogin(c *gin.Context) {
         c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email or password"})
         return
     }
-	token, err := utils.GenerateJWT(body.Email, "CLIENT" )
+	token, err := utils.GenerateJWT(client, "CLIENT" )
 	if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate token"})
         return
     }
 
     c.JSON(http.StatusOK, gin.H{"message": "Login successful", "login_token": token})
+}
+
+func ClientUpdatePassword(c *gin.Context) {
+    var request ClientPasswordUpdateRequest
+
+    if err := c.ShouldBindJSON(&request); err != nil {
+        c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Missing or invalid properties in request body"})
+        return
+    }
+
+    client, exists := c.Get("client")
+    if !exists {
+        c.JSON(http.StatusUnauthorized, ErrorResponse{Error: "Client not authenticated"})
+        return
+    }
+
+    clientModel, ok := client.(models.Client)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error retrieving client information"})
+        return
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(clientModel.Password), []byte(request.OldPassword)); err != nil {
+        c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Incorrect old password"})
+        return
+    }
+
+    hash, err := bcrypt.GenerateFromPassword([]byte(request.NewPassword), bcrypt.DefaultCost)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error updating password"})
+        return
+    }
+
+    clientModel.Password = string(hash)
+    if err := initializers.DB.Save(&clientModel).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error updating password"})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "Update successful"})
+}
+
+func GetUsersByClient(c *gin.Context) {
+    client, exists := c.Get("client")
+    if !exists {
+        c.JSON(http.StatusUnauthorized, ErrorResponse{Error: "Unauthorized"})
+        return
+    }
+
+    clientModel, ok := client.(models.Client)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error retrieving client information"})
+        return
+    }
+
+    var users []models.User
+    if err := initializers.DB.Where("client_id = ?", clientModel.ID).Find(&users).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error fetching users"})
+        return
+    }
+
+    var userResponses []UserResponse
+    for _, user := range users {
+        userResponses = append(userResponses, UserResponse{
+            ID:    user.ID,
+            Email: user.Email,
+			FirstName:  user.FirstName,
+			LastName: user.LastName,
+        })
+    }
+
+    c.JSON(http.StatusOK, userResponses)
+}
+
+func DeleteUserByClient(c *gin.Context) {
+    var request DeleteUserRequest
+
+    if err := c.ShouldBindJSON(&request); err != nil {
+        c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Invalid request body"})
+        return
+    }
+
+    client, exists := c.Get("client")
+    if !exists {
+        c.JSON(http.StatusUnauthorized, ErrorResponse{Error: "Unauthorized"})
+        return
+    }
+
+    clientModel, ok := client.(models.Client)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error retrieving client information"})
+        return
+    }
+
+    var user models.User
+    if err := initializers.DB.Where("id = ? AND client_id = ?", request.UserID, clientModel.ID).First(&user).Error; err != nil {
+        c.JSON(http.StatusBadRequest, ErrorResponse{Error: "User not found or does not belong to this client"})
+        return
+    }
+
+    if err := initializers.DB.Delete(&user).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error deleting user"})
+        return
+    }
+
+    c.JSON(http.StatusOK, DeleteUserResponse{Message: "User deleted successfully"})
+}
+
+func GetClientAPN(c *gin.Context) {
+    client, exists := c.Get("client")
+    if !exists {
+        c.JSON(http.StatusUnauthorized, ErrorResponse{Error: "Unauthorized"})
+        return
+    }
+
+    clientModel, ok := client.(models.Client)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error retrieving client information"})
+        return
+    }
+
+    var clientFromDB models.Client
+    if err := initializers.DB.First(&clientFromDB, clientModel.ID).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error fetching client from database"})
+        return
+    }
+
+    c.JSON(http.StatusOK, GetClientAPNResponse{APN: clientFromDB.APN})
+}
+
+func HandleFeatureRequest(c *gin.Context) {
+    var request FeatureRequest
+
+    if err := c.ShouldBindJSON(&request); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+        return
+    }
+
+    featureRequest := utils.FeatureRequest{
+        FeatureName:        request.FeatureName,
+        FeatureDescription: request.FeatureDescription,
+    }
+
+    if err := utils.SendFeatureRequestEmail( featureRequest); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send email"})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "Feature request email sent successfully"})
+}
+type ClientPasswordUpdateRequest struct {
+    OldPassword string `json:"old_password" binding:"required"`
+    NewPassword string `json:"new_password" binding:"required"`
+}
+type UserResponse struct {
+    ID    uuid.UUID   `json:"id"`
+    Email string `json:"email"`
+    FirstName string `json:"firstname"`
+    LastName string `json:"lastname"`
+}
+
+type DeleteUserResponse struct {
+    Message string `json:"message"`
+}
+type DeleteUserRequest struct {
+    UserID uuid.UUID `json:"user_id" binding:"required"`
+}
+
+type GetClientAPNResponse struct {
+    APN string `json:"apn"`
+}
+
+type FeatureRequest struct {
+    FeatureName        string `json:"feature_name" binding:"required"`
+    FeatureDescription string `json:"feature_description" binding:"required"`
 }
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
+	ginSwagger "github.com/swaggo/gin-swagger"
+    swaggerFiles "github.com/swaggo/files"
 
 	"authapp.com/m/controllers"
 	"authapp.com/m/initializers"
 	"authapp.com/m/middlewares"
 	"github.com/gin-gonic/gin"
+	_ "authapp.com/m/docs" 
+
 )
 
 func init(){
@@ -15,16 +18,47 @@ func init(){
 	// initializers.SyncDatabase()
 }
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     GateKeeper Pro register and validates users for your frontend.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
+
 func main() {
-	fmt.Println("working 1")
 	r := gin.Default()
+	r.Use(middlewares.CORSMiddleware())
+
+	
 	r.POST("/create-client", controllers.ClientSignup)
 	r.POST("/client-login", controllers.ClientLogin)
 	r.POST("/generate-apn",middlewares.ClientAuthMiddleware(), controllers.GenerateAPN )
 	r.POST("/invalidate-apn",middlewares.ClientAuthMiddleware(), controllers.InvalidateAPN )
-
+	r.GET("/all-users",middlewares.ClientAuthMiddleware(), controllers.GetUsersByClient )
+	r.GET("/client-apn",middlewares.ClientAuthMiddleware(), controllers.GetClientAPN )
+	r.POST("/delete-user",middlewares.ClientAuthMiddleware(), controllers.DeleteUserByClient )
+	r.POST("/feature-request",middlewares.ClientAuthMiddleware(), controllers.HandleFeatureRequest)
+	
 	r.POST("/user/signup", middlewares.APNAuthMiddleware(), controllers.UserSignup)
 	r.POST("/user/login", middlewares.APNAuthMiddleware(), controllers.UserLogin)
-	r.POST("/user/validate", middlewares.APNAuthMiddleware(), middlewares.UserAuthMiddleware(), controllers.ValidateUser)
+	r.GET("/user/validate", middlewares.APNAuthMiddleware(), middlewares.UserAuthMiddleware(), controllers.ValidateUser)
+	r.POST("/user/update-password", middlewares.APNAuthMiddleware(), middlewares.UserAuthMiddleware(), controllers.UserUpdatePassword)
+	
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	
 	r.Run()
 }
