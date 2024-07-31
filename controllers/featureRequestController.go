1623842690c24e2d7c387a26a4e8a85d6dc2f12a
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-07-31 13:25:53 +0000

    chore: save feature requests to db

---
+++ controllers/featureRequestController.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"net/http"
+
+	"authapp.com/m/initializers"
+	"authapp.com/m/models"
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func CreateFeatureRequest(c *gin.Context) {
+    var input FeatureRequestInput
+
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    client, exists := c.Get("client")
+    if !exists {
+        c.JSON(http.StatusUnauthorized, ErrorResponse{Error: "Client not authenticated"})
+        return
+    }
+
+    clientModel, ok := client.(models.Client)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Error retrieving client information"})
+        return
+    }
+
+    featureRequest := models.FeatureRequest{
+        Feature: input.Feature,
+        Title:   input.Title,
+		Email: clientModel.Email,
+    }
+
+    if err := initializers.DB.Create(&featureRequest).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save feature request"})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "Feature request submitted successfully"})
+}
+
+type FeatureRequestInput struct {
+    Feature string `json:"feature" binding:"required"`
+    Title   string `json:"title" binding:"required"`
+}
--- models/featureRequest.model.go
+++ models/featureRequest.model.go
@@ -7,20 +7,19 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct {
+type FeatureRequest struct {
 	gorm.Model
     ID        uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-    FirstName string   `gorm:"type:varchar(100);not null"`
-    LastName  string   `gorm:"type:varchar(100);not null"`
+    Feature   string   `gorm:"type:varchar(100);not null"`
+    Title     string   `gorm:"type:varchar(100);not null"`
     Email     string   `gorm:"type:varchar(100);not null"`
-    Password  string   `gorm:"type:varchar(255);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
-    ClientID  uuid.UUID `gorm:"type:uuid"`
-    Client    Client
+
 }
 
-func (User) TableName() string {
-    return "users"
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+    u.ID = uuid.New()
+    return
 }
