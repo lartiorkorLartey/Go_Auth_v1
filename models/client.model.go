5e9568ef0a6dbe7f0101ab80fd862f80cadd6e65
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-08-13 16:27:59 +0000

    chore: update client model with confirmationmethods relationship

--- models/client.model.go
+++ models/client.model.go
@@ -11,36 +11,35 @@ import (
 )
 
 type Client struct {
-    gorm.Model
-    ID                     uuid.UUID           `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-    FirstName              string              `gorm:"type:varchar(100);not null"`
-    LastName               string              `gorm:"type:varchar(100);not null"`
-    Email                  string              `gorm:"type:varchar(100);uniqueIndex;not null"`
-    Password               string              `gorm:"type:varchar(255);not null"`
-    APN                    string              `gorm:"type:varchar(100);uniqueIndex"`
-    ClientAdvancedConfig   ClientAdvancedConfig `gorm:"foreignKey:ClientID"`
-    CreatedAt              time.Time
-    UpdatedAt              time.Time
-    DeletedAt              gorm.DeletedAt      `gorm:"index"`
-    Users                  []User
+	gorm.Model
+	ID                       uuid.UUID                `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	FirstName                string                   `gorm:"type:varchar(100);not null"`
+	LastName                 string                   `gorm:"type:varchar(100);not null"`
+	Email                    string                   `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password                 string                   `gorm:"type:varchar(255);not null"`
+	APN                      string                   `gorm:"type:varchar(100);uniqueIndex"`
+	ClientAdvancedConfig     ClientAdvancedConfig     `gorm:"foreignKey:ClientID"`
+	ClientConfirmationMethod ClientConfirmationMethod `gorm:"foreignKey:ClientID"`
+	CreatedAt                time.Time
+	UpdatedAt                time.Time
+	DeletedAt                gorm.DeletedAt `gorm:"index"`
+	Users                    []User
 }
-
 
 func (c *Client) BeforeCreate(tx *gorm.DB) (err error) {
-    c.ID = uuid.New()
+	c.ID = uuid.New()
 
-    if c.APN, err = GenerateAPN(16); err != nil {
-        return err
-    }
+	if c.APN, err = GenerateAPN(16); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
-
 func GenerateAPN(length int) (string, error) {
-    bytes := make([]byte, length)
-    if _, err := rand.Read(bytes); err != nil {
-        return "", fmt.Errorf("failed to generate random bytes: %w", err)
-    }
-    return hex.EncodeToString(bytes), nil
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", fmt.Errorf("failed to generate random bytes: %w", err)
+	}
+	return hex.EncodeToString(bytes), nil
 }
