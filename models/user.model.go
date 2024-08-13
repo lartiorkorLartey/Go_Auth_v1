77fe54a7aee6f4107df8a905fdecfcbcfa0ecb9a
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-08-13 16:26:37 +0000

    chore: update user model with confirmations relationship

--- models/user.model.go
+++ models/user.model.go
@@ -9,31 +9,33 @@ import (
 
 type User struct {
 	gorm.Model
-    ID        uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-    FirstName string   `gorm:"type:varchar(100);not null"`
-    LastName  string   `gorm:"type:varchar(100);not null"`
-    Email     string   `gorm:"type:varchar(100);not null"`
-    Password  string   `gorm:"type:varchar(255);not null"`
-    AdditionalProperties AdditionalProperties   `gorm:"embedded"`
+	ID                   uuid.UUID            `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	FirstName            string               `gorm:"type:varchar(100);not null"`
+	LastName             string               `gorm:"type:varchar(100);not null"`
+	Email                string               `gorm:"type:varchar(100);not null"`
+	Password             string               `gorm:"type:varchar(255);not null"`
+	AdditionalProperties AdditionalProperties `gorm:"embedded"`
+	UserConfirmation     UserConfirmation     `gorm:"constraint:OnDelete:CASCADE;"`
+
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
-    ClientID  uuid.UUID `gorm:"type:uuid"`
-    Client    Client
+	ClientID  uuid.UUID      `gorm:"type:uuid"`
+	Client    Client
 }
 
 type AdditionalProperties struct {
-    PhoneNumber    *string    `gorm:"type:varchar(20)"`
+	PhoneNumber    *string    `gorm:"type:varchar(20)"`
 	ProfilePicture *string    `gorm:"type:varchar(255)"`
 	DateOfBirth    *time.Time `gorm:"type:date"`
 	Gender         *string    `gorm:"type:varchar(20)"`
 	Address        Address    `gorm:"embedded"`
 	LastLogin      *time.Time `gorm:"type:timestamp"`
-	Role         *string        `gorm:"type:varchar(20)"`
+	Role           *string    `gorm:"type:varchar(20)"`
 }
 
 type Address struct {
-    Street     *string `gorm:"type:varchar(255)"`
+	Street     *string `gorm:"type:varchar(255)"`
 	City       *string `gorm:"type:varchar(100)"`
 	State      *string `gorm:"type:varchar(100)"`
 	PostalCode *string `gorm:"type:varchar(20)"`
@@ -41,6 +43,5 @@ type Address struct {
 }
 
 func (User) TableName() string {
-    return "users"
+	return "users"
 }
-
